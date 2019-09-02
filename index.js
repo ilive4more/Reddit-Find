9a93631898fb1288b88166f2e74022d6f1337b98
James Adams <jimmyadams61@gmail.com>
2019-09-01 17:26:48 -0700

    Removed async await

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ searchForm.addEventListener('submit', e => {
   // Search Reddit
   search(searchTerm, searchLimit, sortBy).then(results => {
     let output = '<div class="card-columns">';
-    console.log(results);
+    // console.log(results);
     results.forEach(post => {
       // Check for image
       let image = post.preview
