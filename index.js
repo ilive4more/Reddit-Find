f893b2a2a6bed70b1cf33d8f84c29a74b0557ec5
James Adams <jimmyadams61@gmail.com>
2019-09-01 17:06:20 -0700

    Added https:

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function search(searchTerm, searchLimit, sortBy) {
   return fetch(
-    `http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`
+    `https://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`
   )
     .then(res => res.json())
     .then(data => data.data.children.map(data => data.data))
