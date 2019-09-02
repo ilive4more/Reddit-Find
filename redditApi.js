f6fb74904068e5941bdd9c5bafe1a70231ec2e2b
James Adams <jimmyadams61@gmail.com>
2019-09-01 19:20:20 -0700

    Removed extra js file

--- redditApi.js
+++
@@ -1,8 +0,0 @@
-function search(searchTerm, searchLimit, sortBy) {
-  return fetch(
-    `http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`
-  )
-    .then(res => res.json())
-    .then(data => data.data.children.map(data => data.data))
-    .catch(err => console.log(err));
-}
